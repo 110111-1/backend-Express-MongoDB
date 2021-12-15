494170a0df133015d603abfaf6cc08070ab83dd8
110111-1 <moty.philippe@free.fr>
2021-12-15 18:54:25 +0100

    End

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -3,17 +3,18 @@ const multer = require('multer');
 const MIME_TYPES = {
     'image/jpg' : 'jpg',
     'image/jpeg': 'jpg',
-    'image/png': 'png'
+    'image/png': 'png',
+    'image/PNG': 'png'
 };
 
 const storage = multer.diskStorage({
     destination: (req, file, callback) => (
         callback(null, 'images')
     ),
-    filename: (req, file, callback) =>{
+    filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_');
         const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.'+ extension);
+        callback(null, name + Date.now() + '.' + extension);
     } 
 });
 
