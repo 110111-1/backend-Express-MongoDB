5efa81e8d6fd06c51a8e25d14b9a410ba99418e2
110111-1 <moty.philippe@free.fr>
2021-12-15 16:54:33 +0100

    ne fonctionne pas ...

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const stuffCtrl = require("../controllers/stuff");
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config')
 
-router.post("/", auth, stuffCtrl.createThing);
+router.get("/", auth, stuffCtrl.getAllThings);
+router.post("/", auth, multer, stuffCtrl.createThing);
 router.put("/:id", auth, stuffCtrl.modifyThing);
 router.delete("/:id", auth, stuffCtrl.deleteThing);
 router.get("/:id", auth, stuffCtrl.getOneThing);
-router.get("/", auth, stuffCtrl.getAllThings);
 
 module.exports = router;
